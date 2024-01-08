1941042077b5c2763cc803e346ac9e19567c26e1
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2024-01-08 13:44:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+function handleInput(input) {
+    const maxLength = parseInt(input.getAttribute('maxlength'));
+
+    if (input.value.length === maxLength) {
+        const nextInput = input.nextElementSibling;
+
+        if (nextInput) {
+            nextInput.focus();
+        }
+    }
+}
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Backspace') {
+        const focusedInput = document.activeElement;
+
+        if (focusedInput.classList.contains('code')) {
+            const previousInput = focusedInput.previousElementSibling;
+
+            if (previousInput) {
+                previousInput.focus();
+                previousInput.value = ''; // Clear the value of the previous input
+            }
+        }
+    }
+});
